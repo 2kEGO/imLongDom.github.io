e2ce33f80098073009cc02bb1fd3f9091bee4097
Long Nguyen <barnonguyen@gmail.com>
2024-10-06 18:59:27 -0400

    update

---
+++ script.js
@@ -0,0 +1,19 @@
+const img = document.getElementById('avatar-img');
+let isZoomed = false;
+let dropDown = document.getElementsByClassName("exp_info_dropdown");
+
+function toggleZoom() {
+    if (isZoomed) {
+        img.style.transform = 'scale(1)';
+        
+    } else {
+        img.style.transform ='scale(1.5)';
+    }
+    img.style.transition = 'transform 0.5s ease';
+    isZoomed = !isZoomed;
+}
+
+function expandExperience(){
+    document.getElementById("dropDown").classList.toggle("show");
+    
+}
