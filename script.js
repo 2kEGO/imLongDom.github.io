1417934e030260efd53689dd2643081b7ecda711
Long Nguyen <barnonguyen@gmail.com>
2024-10-08 21:49:25 -0400

    update

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const img = document.getElementById('avatar-img');
 let isZoomed = false;
 let dropDown = document.getElementsByClassName("exp_info_dropdown");
+const nameInsert = document.getElementById('name_insert');
+const insertSection = document.getElementById('name-insert-section');
+let guestName = document.getElementById('guest_name');
 
+
+// TOGGLE ZOOM AVATAR
 function toggleZoom() {
     if (isZoomed) {
         img.style.transform = 'scale(1)';
@@ -13,6 +18,7 @@ function toggleZoom() {
     isZoomed = !isZoomed;
 }
 
+// EXPERIENCE SHOW TOGGLE
 function expandExperience(){
     document.getElementById('dropDown').classList.toggle("show");
 };
@@ -20,3 +26,15 @@ function expandExperience(){
 function expandExperience2(){    
     document.getElementById('dropDown2').classList.toggle("show");
 };
+
+// SUBMIT NAME welcome page
+function submitName () {
+    const name = nameInsert.value;
+  if (name !== ""){
+        guestName.innerText = `Hi ${name}, My name is Long`;
+        insertSection.style.display = 'none';
+    }else{
+        document.getElementById('userWarning').classList.toggle("show");
+    }
+    
+}
