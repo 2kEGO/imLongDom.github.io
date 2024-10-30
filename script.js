33895c8b1fa366a842fb8c8b937ff54a834dbc47
Long Nguyen <barnonguyen@gmail.com>
2024-10-30 02:36:17 -0400

    update

--- script.js
+++ script.js
@@ -50,3 +50,30 @@ function toggleColorChange() {
 function toggleColorChange2() {
     submitButtonHover.style.backgroundColor = 'Yellow'
 }
+
+// GET CONTACT SECTION
+
+// STOP THE FORM FROM SUBMITTING 
+document.getElementById('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+// GET ALL ID VALUES
+const name = document.getElementById('name').value;
+const email = document.getElementById('email').value;
+const message = document.getElementById('message').value;
+const phone = document.getElementById('phone').value;
+const preferContact = document.getElementById('prefer-contact').value;
+
+// SAVE FORM DATA TO LOCAL STORAGE
+localStorage.setItem('contactFormData', JSON.stringify({
+    name, email, message, phone, preferContact
+}));
+
+// SHOW ALERT FOR SAVED DATA
+alert('Form data saved!');
+});
+
+// CLEAR FORM DATA ON RESET
+document.getElementById('form').addEventListener('reset', function() {
+    alert('Form cleared');
+  });
